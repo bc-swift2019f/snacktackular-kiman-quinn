72694920dfaff84d319178387ceb62509979714b
Kim-An Quinn <kiman.quinn@bc.edu>
2019-10-31 16:51:54 -0400

    9.4 completed: added google place autocomplete

--- Snacktacular/AppDelegate.swift
+++ Snacktacular/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        GMSPlacesClient.provideAPIKey("AIzaSyBsGO15w0qUThhHO5A_2j6NWtcKL2O7Z6A")
         FirebaseApp.configure()
         return true
     }
