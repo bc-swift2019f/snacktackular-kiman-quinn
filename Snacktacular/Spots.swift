7a678f7b1fcbf9cfaddaedaa5dd577a8dc029fb0
Kim-An Quinn <kiman.quinn@bc.edu>
2019-10-30 21:55:50 -0400

    9.2 completed: added tool bars, updated table views, and back button

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,13 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Kim-An Quinn on 10/30/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots{
+    var spotArray = [Spot]()
+}
